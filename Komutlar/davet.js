19892a326ef5300074004c60ae10fd6eb5775a5b
iUgur <77486662+iUgur@users.noreply.github.com>
2021-11-07 19:20:41 +0300

    Update davet.js

--- Komutlar/davet.js
+++ Komutlar/davet.js
@@ -16,9 +16,6 @@ module.exports = {
 
     run: async (client, interaction) => {
 
-let dil = db.fetch(`dil_${interaction.user.id}`)
-
-if(!dil) {
 
 const embed = new Discord.MessageEmbed() 
 
@@ -58,49 +55,6 @@ new Discord.MessageButton()
 
 await interaction.reply({embeds: [embed], ephemeral: true, components: [row]})  
 
-} 
-
-if(dil === "TR") {
-
-const embed = new Discord.MessageEmbed() 
-
-.setDescription("Aşağıdan istediğin linke gidebilir sin!")
-
-.setColor("GREEN")
-
-const row = new MessageActionRow()
-
-			.addComponents(
-
- new Discord.MessageButton() 
-
-.setStyle("LINK") 
-
-.setLabel("Davet linkim") 
-
-.setURL("https://discord.com/oauth2/authorize?client_id=765207268408033322&scope=bot+applications.commands&permissions=2147483656"), 
-
-new Discord.MessageButton() 
-
-.setStyle("LINK") 
-
-.setLabel("Destek sunucum") 
-
-.setURL("https://discord.gg/KZfAEjrPUF"), 
-
-new Discord.MessageButton() 
-
-.setStyle("LINK") 
-
-.setLabel("Oy linkim") 
-
-.setURL("https://top.gg/bot/765207268408033322") 
-
-); 
-
-await interaction.reply({embeds: [embed], ephemeral: true, components: [row]}) 
-
-}
 
 } 
 
